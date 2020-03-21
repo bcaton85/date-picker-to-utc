928ecad1d8e9d464456b16793716b4e0f6ae6089
Brandon Caton <bcaton85@gmail.com>
2020-03-21 13:00:42 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import moment from 'moment';
+import Datetime from 'react-datetime';
 
 function App() {
+
+  const [isoString, setIsoString] = useState(moment().toISOString());
+
+  function onDateInput(e, value){
+    setIsoString(e.toISOString());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Date time to UTC ISO string</h3>
+      <p>A simple converter for getting the ISO string of specifc dates in UTC</p>
+      <p>Datetime:</p>
+      <Datetime className="dateTime"
+        onChange={(e)=>onDateInput(e)}
+        defaultValue={moment(isoString)}
+        />
+      <p>UTC ISO String:</p>
+      <p>{isoString}</p>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/bcaton85/date-picker-to-utc">Github code</a>
     </div>
   );
 }
